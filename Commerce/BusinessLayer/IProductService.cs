73d16c476df5c3e0292c3d285ec67c42b27ff04d
Fatmanur Ozcetin <nurozcetin35@gmail.com>
2025-02-18 19:27:01 +0300

    add question, answers and rate and list them

--- Commerce/BusinessLayer/IProductService.cs
+++ Commerce/BusinessLayer/IProductService.cs
@@ -13,7 +13,6 @@ namespace Commerce.BusinessLayer
         Task<string> AskQuestionAsync(AskDto askDto, string email); //kullanicinin urun hakkinda soru sormasini saglar.
         Task<List<AskDto>> GetQuestionBySellerAsync(int productId); //saticiya urun ustunden gelen sorularin goruntulenmesini saglar.
         Task<string> AnswerQuestionAsync(AskDto askDto, string email); //saticinin sorulan soruya cevap vermesini saglar.
-
         Task<string> RateAsync(RateDto rateDto, string userEmail); //kullanicinin belli bir urun hakkinda degerlendirme yapabilmesini saglar.
         Task<List<Rating>> GetRateByProductAsync(int productId); //belli bir urune ait tum degerlendirmeleri listeler.
     }
